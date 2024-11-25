07d4a260979de97b0f5e0ae0492cc6eaed9b488f
vixhnuchandran <vixhnuchanddran@google.com>
2024-11-25 10:10:02 +0530

    Fix style

--- perplexity.ai/script.js
+++ perplexity.ai/script.js
@@ -10,7 +10,10 @@ function updateTheme() {
   }
 }
 
-window.onload = updateTheme;
+window.onload = () => {
+  updateTheme();
+  setSidebarStateFromLocalStorage();
+};
 
 window
   .matchMedia("(prefers-color-scheme: dark)")
@@ -21,6 +24,23 @@ function toggleSidebar() {
   const collapsedSidebar = document.getElementById("sidebar");
 
   if (extendedSidebar.classList.contains("hidden")) {
+    extendedSidebar.classList.remove("hidden");
+    collapsedSidebar.classList.add("hidden");
+    localStorage.setItem("sidebarState", "extended");
+  } else {
+    extendedSidebar.classList.add("hidden");
+    collapsedSidebar.classList.remove("hidden");
+    localStorage.setItem("sidebarState", "collapsed");
+  }
+}
+
+function setSidebarStateFromLocalStorage() {
+  const sidebarState = localStorage.getItem("sidebarState");
+
+  const extendedSidebar = document.getElementById("extended-sidebar");
+  const collapsedSidebar = document.getElementById("sidebar");
+
+  if (sidebarState === "extended") {
     extendedSidebar.classList.remove("hidden");
     collapsedSidebar.classList.add("hidden");
   } else {
