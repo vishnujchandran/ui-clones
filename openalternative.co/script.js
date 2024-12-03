6c2cb51486ac239d1da38a7ed0d3cd204a461a32
vixhnuchandran <vixhnuchandran@gmail.com>
2024-12-04 02:58:27 +0530

    Add dynamic search bar

--- openalternative.co/script.js
+++ openalternative.co/script.js
@@ -1,4 +1,20 @@
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-if (prefersDarkScheme.matches) {
-  document.body.classList.add("dark");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  if (prefersDarkScheme.matches) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.getElementById("searchButton");
+  const searchInput = document.getElementById("searchInput");
+
+  searchButton.addEventListener("click", function () {
+    searchButton.classList.remove("opacity-100");
+    searchButton.classList.add("opacity-0");
+    searchInput.classList.remove("opacity-0");
+    searchInput.classList.add("opacity-100", "w-32");
+  });
+});
